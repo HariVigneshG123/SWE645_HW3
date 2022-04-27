c39a6c0ceb533dfc58c0981e7b84b5af2ef33919
HariVignesh Gomathi <harivigneshgomathi@HariVigneshs-MacBook-Air.local>
2022-04-27 17:48:48 -0400

    frst commt

---
+++ src/main/java/com/HW3SWE645/HW3SWE645/Hw3Swe645Application.java
@@ -0,0 +1,13 @@
+package com.HW3SWE645.HW3SWE645;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw3Swe645Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw3Swe645Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/HW3SWE645/HW3SWE645/Hw3Swe645ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HW3SWE645.HW3SWE645;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw3Swe645ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
